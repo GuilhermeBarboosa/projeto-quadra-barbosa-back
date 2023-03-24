60100c0d02a59438c7cee61d74256dd25b4a8112
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2023-03-24 07:25:48 -0300

    feat crud de usuario

--- api/models/roles.js
+++ api/models/roles.js
@@ -10,9 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      roles.belongsTo(models.users , {
-        foreignKey: 'role'
-      })
+      // roles.belongsTo(models.users , {
+      //   foreignKey: 'role'
+      // })
     }
   }
   roles.init({
--- api/models/users.js
+++ api/models/users.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       user.hasMany(models.roles, {
-        foreignKey: "role",
+        foreignKey: "id",
       });
     }
   }
